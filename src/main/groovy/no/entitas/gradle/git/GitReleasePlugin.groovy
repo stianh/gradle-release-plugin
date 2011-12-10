eaf82d9033e9b85861141d41f9872ae10700e711
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2011-12-11 00:47:47 +0100

    Remove debug output of plugins registered on current project

--- src/main/groovy/no/entitas/gradle/git/GitReleasePlugin.groovy
+++ src/main/groovy/no/entitas/gradle/git/GitReleasePlugin.groovy
@@ -1,16 +1,11 @@
 package no.entitas.gradle.git
-import no.entitas.gradle.ReleasePlugin;
-import no.entitas.gradle.Version;
-import no.entitas.gradle.git.GitVersion;
 
+import no.entitas.gradle.ReleasePlugin
+import no.entitas.gradle.Version
 import org.gradle.api.Project
-import org.gradle.api.Plugin
-import org.gradle.api.Task;
 
 class GitReleasePlugin extends ReleasePlugin {
-	
 	def void setConvention(Project project) {
-        println("Project plugins is "+project.convention.plugins)
 		project.convention.plugins.gitRelease = new GitReleasePluginConvention();
 	}
 	
