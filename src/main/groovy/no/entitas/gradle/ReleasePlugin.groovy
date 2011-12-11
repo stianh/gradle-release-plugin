5aca4cfbc99b45d2eab433ba55095dde1472e3f2
Stig Kleppe-Jørgensen <from.git@nisgits.net>
2011-12-11 01:52:26 +0100

    Clean up code
    * Formatting
    * Extracted common code into methods

--- src/main/groovy/no/entitas/gradle/ReleasePlugin.groovy
+++ src/main/groovy/no/entitas/gradle/ReleasePlugin.groovy
@@ -5,64 +5,69 @@ import org.gradle.api.Plugin
 import org.gradle.api.Task
 
 abstract class ReleasePlugin implements Plugin<Project> {
-
-	def void apply(Project project) { 
+	def void apply(Project project) {
 		def version = createVersion(project)
 		project.version = version
-		
-		if(project.subprojects.isEmpty()){
-			Task releasePrepareTask = project.task('releasePrepare') << {
-	      		version.releasePrepare()
-			}
-			releasePrepareTask.dependsOn(project.tasks.build)
-		
-			Task performReleaseTask = project.task('releasePerform') << {
-		  		version.releasePerform()
-			}
-			performReleaseTask.dependsOn([releasePrepareTask,project.tasks.uploadArchives]) 	
-		}else{
-			/*TODO: The subprojects closure configuration is not applied at the time when apply is called for this plugin.
-			  The subprojects needs the java plugin at this time to resolve clean, build and the uploadArtifacts tasks.
-			  Investigate if this somehow can be done lazy.
-			*/
-			project.subprojects*.apply plugin: 'java'
 
-			Task cleanAllTask = project.task('cleanAll') << {}		
-			cleanAllTask.dependsOn(project.subprojects*.clean)
-		
-			Task buildAll = project.task('buildAll') << {}
-			buildAll.dependsOn([cleanAllTask, project.subprojects*.build])
-		
-			Task releasePrepareTask = project.task('releasePrepare') << {
+        if (project.subprojects.isEmpty()) {
+            Task releasePrepareTask = project.task('releasePrepare') << {
+                version.releasePrepare()
+            }
+            releasePrepareTask.dependsOn(project.tasks.build)
+
+            Task performReleaseTask = project.task('releasePerform') << {
+                version.releasePerform()
+            }
+            performReleaseTask.dependsOn([releasePrepareTask, project.tasks.uploadArchives])
+        } else {
+            /*
+            TODO: The subprojects closure configuration is not applied at the time when apply is called for this plugin.
+                 The subprojects needs the java plugin at this time to resolve clean, build and the uploadArtifacts tasks.
+                 Investigate if this somehow can be done lazy.
+            */
+            project.subprojects*.apply plugin: 'java'
+
+            Task cleanAllTask = project.task('cleanAll') << {}
+            cleanAllTask.dependsOn(project.subprojects*.clean)
+
+            Task buildAll = project.task('buildAll') << {}
+            buildAll.dependsOn([cleanAllTask, project.subprojects*.build])
+
+            Task releasePrepareTask = project.task('releasePrepare') << {
                 def dependencies = getSnapshotDependencies(project)
+
                 if (!dependencies.isEmpty()) {
-                  throw new RuntimeException('Project contains SNAPSHOT dependencies: ' + dependencies)
+                    throw new RuntimeException('Project contains SNAPSHOT dependencies: ' + dependencies)
                 }
-	      		version.releasePrepare()
-			}
-			releasePrepareTask.dependsOn(buildAll)
-		
-			Task performReleaseTask = project.task('releasePerform') << {
-		  		version.releasePerform()
-			}
-			performReleaseTask.dependsOn([releasePrepareTask,project.subprojects*.uploadArchives]) 	
-		}
-	}
+
+                version.releasePrepare()
+            }
+            releasePrepareTask.dependsOn(buildAll)
+
+            Task performReleaseTask = project.task('releasePerform') << {
+                version.releasePerform()
+            }
+            performReleaseTask.dependsOn([releasePrepareTask, project.subprojects*.uploadArchives])
+        }
+    }
 
     def getSnapshotDependencies(def project) {
         def deps = [] as Set
+
         project.allprojects {
             it.configurations.all {
-                it.resolvedConfiguration.resolvedArtifacts.each { a ->
-                    def resolvedDep = a.resolvedDependency
-                    if (resolvedDep.moduleVersion.contains("SNAPSHOT")) {
-                        deps.add("${resolvedDep.moduleGroup}:${resolvedDep.moduleName}:${resolvedDep.moduleVersion}")
+                it.resolvedConfiguration.resolvedArtifacts.each { artifact ->
+                    def dependency = artifact.resolvedDependency
+
+                    if (dependency.moduleVersion.contains("SNAPSHOT")) {
+                        deps.add("${dependency.moduleGroup}:${dependency.moduleName}:${dependency.moduleVersion}")
                     }
                 }
             }
         }
+
         deps
     }
 	
-	abstract def Version createVersion(Project project);
+	abstract def Version createVersion(Project project)
 }
--- src/main/groovy/no/entitas/gradle/git/GitVersion.groovy
+++ src/main/groovy/no/entitas/gradle/git/GitVersion.groovy
@@ -1,14 +1,13 @@
 package no.entitas.gradle.git
 
-import no.entitas.gradle.Version;
-import no.entitas.gradle.VersionNumber;
-
+import no.entitas.gradle.Version
+import no.entitas.gradle.VersionNumber
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.process.internal.ExecException
 
 class GitVersion implements Version {
-    private final Project project
+    Project project
     String versionNumber
     Boolean release = null
     def releaseTagPattern = ~/^\S+-REL-\d+$/
@@ -16,24 +15,23 @@ class GitVersion implements Version {
     def GitVersion(project) {
         this.project = project
 
-        project.gradle.taskGraph.whenReady {graph ->
+        project.gradle.taskGraph.whenReady { graph ->
             if (graph.hasTask(':releasePrepare')) {
-				releasePreConditions()
+                releasePreConditions()
                 release = true
-                this.versionNumber = getNextTagName()
-            }
-            else if (isOnReleaseTag() && !hasLocalModifications()) {
+                versionNumber = getNextTagName()
+            } else if (isOnReleaseTag() && !hasLocalModifications()) {
                 release = true
-                this.versionNumber = getCurrentVersion()
-            }
-            else {
+                versionNumber = getCurrentVersion()
+            } else {
                 release = false
-                this.versionNumber = getCurrentBranchName() + '-SNAPSHOT'
+                versionNumber = getCurrentBranchName() + '-SNAPSHOT'
             }
         }
     }
-    //TODO:Ensure that we are on a branch
-	def releasePreConditions(){
+
+    // TODO Ensure that we are on a branch
+    def releasePreConditions() {
         if (hasLocalModifications()) {
             throw new RuntimeException('Uncommited changes found in the source tree:\n' + getLocalModifications())
         }
@@ -43,14 +41,13 @@ class GitVersion implements Version {
         }
 
 
-        if(branchIsAheadOfRemote()){
+        if (branchIsAheadOfRemote()) {
             throw new RuntimeException('Project contains unpushed commits');
         }
-
-	}
+    }
 
     boolean branchIsAheadOfRemote() {
-       return gitExec(['status']).contains('Your branch is ahead of')
+        gitExec(['status']).contains('Your branch is ahead of')
     }
 
     String toString() {
@@ -61,71 +58,75 @@ class GitVersion implements Version {
         if (release == null) {
             throw new GradleException("Can't determine whether this is a release build before the task graph is populated")
         }
-        return release
+
+        release
     }
 
-
     def releasePrepare() {
-        def newTag = versionNumber
-        tag(newTag, "Release Tag: ${newTag}")
+        tag(versionNumber, "Release Tag: ${versionNumber}")
     }
 
     def releasePerform() {
         pushTags()
     }
 
+    def getLocalModifications() {
+        gitExec(['status', '--porcelain'])
+    }
 
-	def getLocalModifications() {
-	    gitExec(['status', '--porcelain'])
-	}
     def hasLocalModifications() {
-      getLocalModifications() == null ? false : true
+        getLocalModifications() != null
     }
 
     def isOnReleaseTag() {
         try {
-            def gitResult = gitExec(['describe', '--exact-match', 'HEAD'], true)
-            return releaseTagPattern.matcher(gitResult).matches()
+            releaseTagPattern.matcher(tagNameOnCurrentRevision()).matches()
         } catch (ExecException e) {
-            return false
+            false
         }
     }
 
     def getCurrentVersion() {
-        try{
-            def result = gitExec(['describe', '--exact-match', 'HEAD'],true)
-             new VersionNumber(result)
+        try {
+            new VersionNumber(tagNameOnCurrentRevision())
         } catch (ExecException e) {
             throw new RuntimeException("Not on a tag.")
         }
     }
 
+    def tagNameOnCurrentRevision() {
+        gitExec(['describe', '--exact-match', 'HEAD'], true)
+    }
+
     def getCurrentBranchName() {
-        gitExec(['name-rev', '--name-only', 'HEAD'],true)
+        gitExec(['name-rev', '--name-only', 'HEAD'], true)
     }
 
     def getNextTagName() {
         def currentBranch = getCurrentBranchName()
         def latestTagVersion = getLatestTag(currentBranch)
+
         if (latestTagVersion == null) {
-			return "$currentBranch-REL-1"
+            "$currentBranch-REL-1"
+        } else {
+            latestTagVersion.nextVersionTag()
         }
-		latestTagVersion.nextVersionTag()
     }
 
     def getLatestTag(String currentBranch) {
         def tagSearchPattern = "${currentBranch}-REL-*"
         def tags = getTagNames(tagSearchPattern)
-        
-		if (tags != null) {
-			def versionNumbers = tags.collect{new VersionNumber(it)}
-            groovy.util.GroovyCollections.max(versionNumbers)
+
+        if (tags != null) {
+            def versionNumbers = tags.collect {new VersionNumber(it)}
+            GroovyCollections.max(versionNumbers)
         }
     }
 
     def getTagNames(String tagSearchPattern) {
         def allReleaseTagNames = gitExec(['tag', '-l', tagSearchPattern])
-        if(allReleaseTagNames){
+        
+        if (allReleaseTagNames) {
             allReleaseTagNames.split('\n')
         }
     }
@@ -142,15 +143,18 @@ class GitVersion implements Version {
 
     def gitExec(List gitArgs, boolean removeNewLines = false) {
         def stdout = new ByteArrayOutputStream()
+
         project.exec {
             executable = 'git'
             args = gitArgs
             standardOutput = stdout
         }
+
         if (stdout.toByteArray().length > 0) {
             def result = stdout.toString()
+
             if (removeNewLines) {
-                stdout.toString().replaceAll("\\n", "")
+                result.replaceAll("\\n", "")
             } else {
                 result
             }
